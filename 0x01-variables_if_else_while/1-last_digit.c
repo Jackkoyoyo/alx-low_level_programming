75beba2ac6182e87488246e0cf6a772682eea916
Jackkoyoyo <Jackkoyoyo@gmail.com>
2023-12-27 19:21:23 -0800

    Correction of last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int pn = n % 10;
+
+	int pn;
+
+	pn = n % 10;
 
 	if (pn != 0 && pn < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, pn);
