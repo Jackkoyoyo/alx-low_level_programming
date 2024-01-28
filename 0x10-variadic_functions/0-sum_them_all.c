462961c574735bbd26ea18402acc54c6aec18f11
Jackkoyoyo <Jackkoyoyo@gmail.com>
2024-01-28 08:26:20 -0800

    1st commit of VF

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all of its parameters
+ * @n: variable arguments count
+ *
+ * Return: sum of all variable arguments
+ * 0 if n is zero
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int me = 0;
+	int us = 0;
+	va_list add;
+
+/*if constant int n is not 0 or less*/
+	if (n > 0)
+	{
+		va_start(add, n);
+
+		while (me < n)
+/* perform addition of arguments pass(integers)*/
+		{
+			us += va_arg(add, int);
+			me++;
+		}
+		va_end(add);
+	}
+	return (us);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/** struct format - Struct for format specifier
+ * @char: specifier
+ * @fp: data type pointer
+ */
+typedef struct format
+{
+	char spc;
+	void (*fp)(void *);
+} frmt;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /*VARIADIC_FUNCTIONS_H*/
